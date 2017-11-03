1761e16cd484fb0a2a775d619afd78462c14c5ae
Nick Whitehead <flyingVee75@gmail.com>
2017-11-03 15:08:34 +0000

    Working test to exercise the API, but commented out as URL not committed to public repo

---
+++ Healthshare/HealthshareTests/RequestHeartRateDataSetTests.m
@@ -0,0 +1,33 @@
+//
+//  RequestHeartRateDataSetTests.m
+//  HealthshareTests
+//
+//  Created by Nick Whitehead on 03/11/2017.
+//  Copyright © 2017 Mobetrics Limited. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "HeartRateDataSet.h"
+
+@interface RequestHeartRateDataSetTests : XCTestCase
+
+@end
+
+@implementation RequestHeartRateDataSetTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testRequest {
+    //NSURL *url = [NSURL URLWithString:@"****"];
+    //NSData *data = [NSData dataWithContentsOfURL:url];
+    //HeartRateDataSet* heartRateDataSet = [[HeartRateDataSet alloc] initWithData:data];
+    //XCTAssertEqual(210507, [heartRateDataSet count]);
+}
+
+@end
